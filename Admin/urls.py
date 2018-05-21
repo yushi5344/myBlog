5c938213556ed35bcd818924c389e5113f049097
guomin <yushi5344@gmail.com>
2018-05-21 21:43:00 +0800

    图片修改

--- Admin/urls.py
+++ Admin/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('image_add/', image.image_add),
     path('upload/', image.upload),
     re_path('image_delete-(?P<id>\d+)', image.image_delete),
+    re_path('image_edit-(?P<id>\d+)', image.image_edit),
     re_path('image_changestate-(?P<id>\d+)-(?P<status>\d+)', image.image_changestate),
 ]
--- Admin/views/image.py
+++ Admin/views/image.py
@@ -34,23 +34,42 @@ def image_add(request):
         cate=Cate.objects.filter(p_id=1)
         return render(request,'Admin/image_add.html',{'cate':cate})
     else:
-        try:
-            title=request.POST.get('title',None)
-            tags=request.POST.get('tags',None)
-            c_id=request.POST.get('c_id',None)
-            sort=request.POST.get('sort',None)
-            allow_comment=request.POST.get('allow_comment',None)
-            up_time=request.POST.get('up_time',None)
-            down_time=request.POST.get('down_time',None)
-            contents=request.POST.get('contents',None)
-            thumb=request.POST.get('thumb',None)
-            img_url=request.POST.get('img_url',None)
-            obj=mImage(
+        id=request.POST.get('id',None)
+        title = request.POST.get('title', None)
+        tags = request.POST.get('tags', None)
+        c_id = request.POST.get('c_id', None)
+        sort = request.POST.get('sort', None)
+        allow_comment = request.POST.get('allow_comment', 0)
+        up_time = request.POST.get('up_time', None)
+        down_time = request.POST.get('down_time', None)
+        contents = request.POST.get('contents', None)
+        thumb = request.POST.get('thumb', None)
+        if not id:
+            try:
+                img_url=request.POST.get('img_url',None)
+                obj=mImage(
+                    title=title,
+                    tags=tags,
+                    img_url=img_url,
+                    allow_comment=allow_comment,
+                    status=0,
+                    up_time=up_time,
+                    down_time=down_time,
+                    thumb=thumb,
+                    contents=contents,
+                    sort=sort,
+                    c_id=c_id
+                )
+                obj.save()
+                ret = {'status': True,  'msg': '保存成功'}
+            except Exception as e:
+                ret = {'status': False, 'msg': '保存失败'}
+            return HttpResponse(json.dumps(ret))
+        else:
+            result=mImage.objects.filter(id=id).update(
                 title=title,
                 tags=tags,
-                img_url=img_url,
                 allow_comment=allow_comment,
-                status=0,
                 up_time=up_time,
                 down_time=down_time,
                 thumb=thumb,
@@ -58,11 +77,8 @@ def image_add(request):
                 sort=sort,
                 c_id=c_id
             )
-            obj.save()
-            ret = {'status': True,  'msg': '保存成功'}
-        except Exception as e:
-            ret = {'status': False, 'msg': '保存失败'}
-        return HttpResponse(json.dumps(ret))
+            ret = {'status': True, 'msg': '修改成功'}
+            return HttpResponse(json.dumps(ret))
 
 def upload(request):
     ret={'status':False,'path':None,'msg':None}
@@ -91,3 +107,9 @@ def image_changestate(request,id,status):
     result=mImage.objects.filter(id=id).update(status=status)
     ret = {'status': True, 'msg': '修改成功'}
     return HttpResponse(json.dumps(ret))
+
+def image_edit(request,id):
+    cate = Cate.objects.filter(p_id=1)
+    image=mImage.objects.filter(id=id).first()
+    edit=1
+    return render(request,'Admin/image_add.html',{'image':image,'cate':cate,'edit':edit})
