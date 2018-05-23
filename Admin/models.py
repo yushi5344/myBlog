bcac0b9c5ad6eaa47b4034876e416319b950b7a7
guomin <yushi5344@gmail.com>
2018-05-23 21:47:40 +0800

    文章数据库字段

--- Admin/models.py
+++ Admin/models.py
@@ -42,3 +42,22 @@ class Image(models.Model):
     contents=models.CharField(max_length=255)
     sort=models.IntegerField(default=1)
 
+#文章
+class Article(models.Model):
+    id=models.AutoField(primary_key=True)
+    c = models.ForeignKey('Cate', to_field='id', on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=40)
+    tags = models.CharField(max_length=255)
+    allow_comment = models.BooleanField(blank=True)
+    add_time = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.IntegerField(default=0)
+    up_time = models.DateTimeField(null=True)
+    down_time = models.DateTimeField(null=True)
+    thumb = models.TextField(null=True)
+    describe = models.CharField(max_length=255)
+    sort = models.IntegerField(default=1)
+    author=models.CharField(max_length=50)
+    source=models.CharField(max_length=50)
+    type=models.IntegerField(null=True)
+    content = models.TextField(null=True)
+
